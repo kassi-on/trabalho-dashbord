ca1dbc7ec8db79329cf4b5a15c937a7c9e4b45ef
Kassio <kassio@unifev.edu.br>
2026-03-06 21:49:42 -0300

    dashboard tarefa

---
+++ dashboard.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="dash.css">
+</head>
+<body>
+    <div class="dashboard-container">
+        <nav>
+            <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Relatórios</a></li>
+                <li><a href="#">Configurações</a></li>
+            </ul>
+
+            <div class="perfil-usuario">
+                <img src="https://ui-avatars.com/api/?name=Kassio+Adriano&background=008080&color=fff" alt="Avatar">
+                <span>Kássio</span>
+            </div>
+        </nav>
+        <main>
+            <section class="container-cards">
+                
+                <article>
+                    <h2>Módulo de Usuários</h2>
+                    <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+                    <a href="#" class="btn">Acessar</a>
+                </article>
+
+                <article>
+                    <h2>Acessas dados pessoais</h2>
+                    <p>verifique se todos os seus dados se estão corretos.</p>
+                    <a href="#" class="btn">Acessar</a>
+                </article>
+
+                <article>
+                    <h2>Relatórios</h2>
+                    <p>Visualize estatísticas e métricas detalhadas do sistema.</p>
+                    <a href="#" class="btn">Acessar</a>
+                </article>
+
+            </section>
+        </main>
+    </div>
+    <footer>
+        <p>&copy; 2026 Desenvolvido na aula de web I, por Kássio Adriano</p>
+    </footer>
+
+</body>
+</html>
