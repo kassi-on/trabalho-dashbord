fd1722efb616c39606a31757ad0062290c101462
Kassio <kassio@unifev.edu.br>
2026-03-09 22:26:04 -0300

    feat: implementada tabela de gestão de usuários

---
+++ usuarios.php
@@ -0,0 +1,49 @@
+<head>
+<link rel="stylesheet" href="dash.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<main>
+    <div class= "header-content">
+        <h2>Gestão de Usuários</h2>
+        <p>Vizualize e grencie as permissões dos usuários do sistema.</p>
+    </div>
+    
+    <div class="table-responsive"> 
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Acesso</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>01</td>
+                <td>Kássio Adriano</td>
+                <td>kassio@unifev.edu.br</td>
+                <td>Admnistrador</td>
+                <td><span class="badge ativo">Ativo</span></td>
+                <td><button class="btn-icon"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="btn-icon"><i class="fa-solid fa-trash"></i></button>
+                    <button class="btn-icon"><i class="fa-solid fa-eye"></i></button>
+                </td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>Adriano</td>
+                <td>adriano.silva@emaiil.com</td>
+                <td>Editor</td>
+                <td>Ativo</td>
+                <td><button class="btn-icon"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="btn-icon"><i class="fa-solid fa-trash"></i></button>
+                    <button class="btn-icon"><i class="fa-solid fa-eye"></i></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+</main>
